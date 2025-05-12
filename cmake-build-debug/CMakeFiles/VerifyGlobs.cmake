4adcf2487ea7d03fb9e6e21061797d8dd6d1a017
Adam Simonov <adam.simonov@gmail.com>
2025-05-12 19:48:31 +0300

    -updated cmake so SFML works on mac

--- cmake-build-debug/CMakeFiles/VerifyGlobs.cmake
+++ cmake-build-debug/CMakeFiles/VerifyGlobs.cmake
@@ -3,7 +3,7 @@
 cmake_policy(SET CMP0009 NEW)
 
 # MY_HEADER_FILES at include/CMakeLists.txt:2 (file)
-file(GLOB NEW_GLOB LIST_DIRECTORIES false RELATIVE "/Users/adamsimonov/לימודים/OOP2/oop2_ex04 - adam_simonov-benny_beer/include" "/Users/adamsimonov/לימודים/OOP2/oop2_ex04 - adam_simonov-benny_beer/include/*.h")
+file(GLOB NEW_GLOB LIST_DIRECTORIES false RELATIVE "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/include" "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/include/*.h")
 set(OLD_GLOB
   "Board.h"
   "Enemy.h"
@@ -16,15 +16,22 @@ set(OLD_GLOB
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
-  file(TOUCH_NOCREATE "/Users/adamsimonov/לימודים/OOP2/oop2_ex04 - adam_simonov-benny_beer/cmake-build-debug/CMakeFiles/cmake.verify_globs")
+  file(TOUCH_NOCREATE "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/cmake-build-debug/CMakeFiles/cmake.verify_globs")
 endif()
 
 # MY_SOURCE_FILES at src/CMakeLists.txt:1 (file)
-file(GLOB_RECURSE NEW_GLOB LIST_DIRECTORIES false RELATIVE "/Users/adamsimonov/לימודים/OOP2/oop2_ex04 - adam_simonov-benny_beer/src" "/Users/adamsimonov/לימודים/OOP2/oop2_ex04 - adam_simonov-benny_beer/src/*.cpp")
+file(GLOB_RECURSE NEW_GLOB LIST_DIRECTORIES false RELATIVE "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/src" "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/src/*.cpp")
 set(OLD_GLOB
-  "main.cpp"
+  "Board.cpp"
+  "Enemy.cpp"
+  "FileParser.cpp"
+  "FloodFill.cpp"
+  "GameController.cpp"
+  "Hud.cpp"
+  "Player.cpp"
+  "WindowManager.cpp"
   )
 if(NOT "${NEW_GLOB}" STREQUAL "${OLD_GLOB}")
   message("-- GLOB mismatch!")
-  file(TOUCH_NOCREATE "/Users/adamsimonov/לימודים/OOP2/oop2_ex04 - adam_simonov-benny_beer/cmake-build-debug/CMakeFiles/cmake.verify_globs")
+  file(TOUCH_NOCREATE "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/cmake-build-debug/CMakeFiles/cmake.verify_globs")
 endif()
