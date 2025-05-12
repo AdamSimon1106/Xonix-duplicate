5b9dec31ffaf5a72145a2dc974a1c405ec3ad307
Adam Simonov <adam.simonov@gmail.com>
2025-05-12 19:48:38 +0300

    -updated cmake so SFML works on mac

---
+++ cmake-build-debug/CMakeFiles/oop2_ex04.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/src/Board.cpp" "CMakeFiles/oop2_ex04.dir/src/Board.cpp.o" "gcc" "CMakeFiles/oop2_ex04.dir/src/Board.cpp.o.d"
+  "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/src/Enemy.cpp" "CMakeFiles/oop2_ex04.dir/src/Enemy.cpp.o" "gcc" "CMakeFiles/oop2_ex04.dir/src/Enemy.cpp.o.d"
+  "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/src/FileParser.cpp" "CMakeFiles/oop2_ex04.dir/src/FileParser.cpp.o" "gcc" "CMakeFiles/oop2_ex04.dir/src/FileParser.cpp.o.d"
+  "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/src/FloodFill.cpp" "CMakeFiles/oop2_ex04.dir/src/FloodFill.cpp.o" "gcc" "CMakeFiles/oop2_ex04.dir/src/FloodFill.cpp.o.d"
+  "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/src/GameController.cpp" "CMakeFiles/oop2_ex04.dir/src/GameController.cpp.o" "gcc" "CMakeFiles/oop2_ex04.dir/src/GameController.cpp.o.d"
+  "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/src/Hud.cpp" "CMakeFiles/oop2_ex04.dir/src/Hud.cpp.o" "gcc" "CMakeFiles/oop2_ex04.dir/src/Hud.cpp.o.d"
+  "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/src/Player.cpp" "CMakeFiles/oop2_ex04.dir/src/Player.cpp.o" "gcc" "CMakeFiles/oop2_ex04.dir/src/Player.cpp.o.d"
+  "/Users/adamsimonov/לימודים/OOP2/oop2-ex04/src/WindowManager.cpp" "CMakeFiles/oop2_ex04.dir/src/WindowManager.cpp.o" "gcc" "CMakeFiles/oop2_ex04.dir/src/WindowManager.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/oop2_ex04.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/oop2_ex04.dir/src/Board.cpp.o"
+  "CMakeFiles/oop2_ex04.dir/src/Board.cpp.o.d"
+  "CMakeFiles/oop2_ex04.dir/src/Enemy.cpp.o"
+  "CMakeFiles/oop2_ex04.dir/src/Enemy.cpp.o.d"
+  "CMakeFiles/oop2_ex04.dir/src/FileParser.cpp.o"
+  "CMakeFiles/oop2_ex04.dir/src/FileParser.cpp.o.d"
+  "CMakeFiles/oop2_ex04.dir/src/FloodFill.cpp.o"
+  "CMakeFiles/oop2_ex04.dir/src/FloodFill.cpp.o.d"
+  "CMakeFiles/oop2_ex04.dir/src/GameController.cpp.o"
+  "CMakeFiles/oop2_ex04.dir/src/GameController.cpp.o.d"
+  "CMakeFiles/oop2_ex04.dir/src/Hud.cpp.o"
+  "CMakeFiles/oop2_ex04.dir/src/Hud.cpp.o.d"
+  "CMakeFiles/oop2_ex04.dir/src/Player.cpp.o"
+  "CMakeFiles/oop2_ex04.dir/src/Player.cpp.o.d"
+  "CMakeFiles/oop2_ex04.dir/src/WindowManager.cpp.o"
+  "CMakeFiles/oop2_ex04.dir/src/WindowManager.cpp.o.d"
+  "oop2_ex04"
+  "oop2_ex04.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/oop2_ex04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
