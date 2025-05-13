840371dc95b2cf5b685fb9dca2b78be88e1b6dd3
Benny Beer <bennybe@std.hac.ac.il>
2025-05-13 18:07:28 +0300

    Add .gitignore file

--- out/build/x64-Debug/ZipBackup.cmake
+++
@@ -1,5 +0,0 @@
-string (TIMESTAMP MY_TIMESTAMP "%Y-%m-%d-%H-%M-%S")
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe --build "C:/Users/ben beer/Desktop/oop2/oop2-ex04/out/build/x64-Debug" --target package_source)
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E copy "C:/Users/ben beer/Desktop/oop2/oop2-ex04/out/build/x64-Debug/oop2_ex04-Adam_Simonov-Benny_Beer.zip" "C:/Users/ben beer/Desktop/oop2/oop2_ex04-Adam_Simonov-Benny_Beer-${MY_TIMESTAMP}.zip")
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E echo "")
-execute_process (COMMAND C:/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/CommonExtensions/Microsoft/CMake/CMake/bin/cmake.exe -E echo "***" C:/Users/ben beer/Desktop/oop2/oop2_ex04-Adam_Simonov-Benny_Beer-${MY_TIMESTAMP}.zip "file created ***")
